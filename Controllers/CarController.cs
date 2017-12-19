ed82733945308774b2b5ba4ce764e39404d30d53
Vinicius Tode <viniciustodesco@gmail.com>
2017-12-19 01:06:11 -0200

    first commit

---
+++ Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DocApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+public class CarController : Controller
+{
+
+    [HttpGet("{Name}")]
+    public CarModel Get(string Name)
+    {
+        return new CarModel
+        {
+            Name = Name
+        };  
+    }
+}
---
+++ Models/CarModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DocApi.Models
+{
+    public class CarModel
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Manufacturer
+        {
+            get
+            {
+                if (Name.ToUpper() == "CIVIC")
+                    return "Honda";
+                if (Name.ToUpper() == "CRUZE")
+                    return "Chevrolet";
+                return "-";
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+    }
+}
